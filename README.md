54381ea33b8979365f74a8594edda7ffb739f28c
dropshape <admin@dropshape.com>
2013-12-19 01:08:50 +0000

    Moved To Promise API

---
+++ README.md
@@ -0,0 +1,77 @@
+###MongooseURLShortener
+
+Please Share on Twitter if you like #MongooseURLShortener
+
+<a href="https://twitter.com/intent/tweet?hashtags=MongooseURLShortener&amp;&amp;text=Check%20out%20this%20repo%20on%20github&amp;tw_p=tweetbutton&amp;url=https%3A%2F%2Fgithub.com%2Fdropshape&amp;via=dropshape" style="float:right">
+<img src="https://raw.github.com/dropshape/MongooseURLShortener/master/twittershare.png">
+</a>
+
+A simple URL Shortening library for NodeJS using [Promises/A+](http://promises-aplus.github.io/promises-spec/) results.
+
+####Installation
+
+    npm install mongoose-url-shortener --save
+
+####API
+    
+    //Setup Mongoose
+    var connection = require('mongoose').connect('mongodb:testing');
+    //Initialize Shortener
+    var urlShotener = MongooseURLShortener(connection, options);
+
+#####Options
+    {
+        seed:12345,
+        schema: {
+            customSchemaValue:Number,
+            customSchemaObj:{}
+        }
+    }
+**Seed**
+Optional value to use for generating short url's. You must always use the same seed otherwise you will not be able to resolve short urls' back to their original url.
+
+**Schema**
+Optional schema properties for the Mongoose object. You can then pass the extra values you define here to be saved along with the URL when shortening or resolving
+
+#####Shorten
+Shorten will either create a new short url in your database or resolve to a previously shortened URL.
+
+    url = 'http://www.google.com';
+    data = { 'any data you want to record with the short url' : true }
+    var promise = urlShortener.shorten(url, data);
+    promise.then(function(url){
+        console.log(url.hash);
+        //aGasjn1Ho
+    }).fail(function(err){
+        console.error('Error creating short url', err);
+    });
+
+#####Resolve
+Resolve will return the original URL before shortening if one is available in the database.
+
+    url = 'aGasjn1Ho';
+    data = {ip:'127.0.0.1'};
+    var result = urlShotener.resolve(url, data);
+    result.then(function(url){
+    console.log(url );
+    // { 
+    //    type: 'MongooseURLShortener', 
+    //    url: 'http://www.google.com', 
+    //    hash:'aGasjn1Ho',
+    //    hits:{
+    //        {ip:'127.0.0.1},
+    //        {ip:'127.0.0.1},
+    //    },
+    //    totalHits: 2
+    //}
+    }).fail(function(err){});
+
+As you can see each time you resolve a url your data passed to the object will be added to an array of hits that way you can record data or analytics each time the url is accessed. 
+
+Check out [NodeTinyUrl](https://github.com/dropshape/NodeTinyUrl) for a working implementation
+
+Please Share on Twitter if you like #MongooseURLShortener
+
+<a href="https://twitter.com/intent/tweet?hashtags=MongooseURLShortener&amp;&amp;text=Check%20out%20this%20repo%20on%20github&amp;tw_p=tweetbutton&amp;url=https%3A%2F%2Fgithub.com%2Fdropshape&amp;via=dropshape" style="float:right">
+<img src="https://raw.github.com/dropshape/MongooseURLShortener/master/twittershare.png">
+</a>
--- test/Index.spec.js
+++ test/Index.spec.js
@@ -23,74 +23,112 @@ describe('MongooseExtension Tests', function () {
     describe('SHOULD', function () {
 
         it('Add userId to model schema', function (done) {
-            urlShortener.shorten('http://www.google.com', {userId: 'google'}, function (err, res) {
-                expect(res.hash).toBeDefined();
-                expect(res.userId).toBe('google');
-                expect(res.url).toBe('http://www.google.com');
-                done(err);
-            });
+            urlShortener.shorten('http://www.google.com', {userId: 'google'})
+                .then(function (res) {
+                    expect(res.hash).toBeDefined();
+                    expect(res.userId).toBe('google');
+                    expect(res.url).toBe('http://www.google.com');
+                    done();
+                })
+                .fail(function (err) {
+                    done(err);
+                });
         });
 
         it('Shorten a URL', function (done) {
-            urlShortener.shorten('http://www.google.com', function (err, res) {
-                expect(res.hash).toBeDefined();
-                expect(res.url).toBe('http://www.google.com');
-                done(err);
-            });
+            urlShortener.shorten('http://www.google.com')
+                .then(function (res) {
+                    expect(res.hash).toBeDefined();
+                    expect(res.url).toBe('http://www.google.com');
+                    done();
+                }).fail(function (err) {
+                    done(err);
+                });
         });
 
         it('Be able to find a shortened URL', function (done) {
-            urlShortener.shorten('http://www.google.com', function (err, res) {
-                expect(res.hash).toBeDefined();
-                expect(res.url).toBe('http://www.google.com');
-                urlShortener.resolve(res.hash, function (err, res) {
+            urlShortener.shorten('http://www.google.com')
+                .then(function (res) {
                     expect(res.hash).toBeDefined();
                     expect(res.url).toBe('http://www.google.com');
-                    expect(res.hits.length).toBe(1);
+                    return res;
+                })
+                .then(function (res) {
+                    urlShortener.resolve(res.hash)
+                        .then(function (res) {
+                            expect(res.hash).toBeDefined();
+                            expect(res.url).toBe('http://www.google.com');
+                            expect(res.hits.length).toBe(1);
+                            done();
+                        }).fail(function (err) {
+                            done(err);
+                        });
+                })
+                .fail(function (err) {
                     done(err);
                 });
-            });
         });
 
         it('Return the same url if it already exists', function (done) {
-            urlShortener.shorten('http://www.google.com', function (err, first) {
-                expect(first.hash).toBeDefined();
-                expect(first.url).toBe('http://www.google.com');
-                urlShortener.shorten('http://www.google.com', function (err, second) {
-                    expect(second.hash).toBeDefined();
-                    expect(second.url).toBe('http://www.google.com');
-                    expect(first._id.toString()).toBe(second._id.toString());
+            urlShortener.shorten('http://www.google.com')
+                .then(function (first) {
+                    expect(first.hash).toBeDefined();
+                    expect(first.url).toBe('http://www.google.com');
+                    return first;
+                })
+                .then(function (first) {
+                    urlShortener.shorten('http://www.google.com')
+                        .then(function (second) {
+                            expect(second.hash).toBeDefined();
+                            expect(second.url).toBe('http://www.google.com');
+                            expect(first._id.toString()).toBe(second._id.toString());
+                            done();
+                        })
+                        .fail(function (err) {
+                            done(err);
+                        });
+                })
+                .fail(function (err) {
                     done(err);
                 });
-            });
         });
 
         it('Hold information about the user that requests the shortened url', function (done) {
-            urlShortener.shorten('http://www.google.com', function (err, first) {
-                expect(first.hash).toBeDefined();
-                expect(first.url).toBe('http://www.google.com');
-                urlShortener.resolve(first.hash, {ip: '127.0.0.1', realm: 'twitter'}, function (err, second) {
-                    expect(second.hash).toBeDefined();
-                    expect(second.url).toBe('http://www.google.com');
-                    expect(second.hits[0]).toEqual({ ip: '127.0.0.1', realm: 'twitter' });
+            urlShortener.shorten('http://www.google.com')
+                .then(function (first) {
+                    expect(first.hash).toBeDefined();
+                    expect(first.url).toBe('http://www.google.com');
+                    urlShortener.resolve(first.hash, {ip: '127.0.0.1', realm: 'twitter'})
+                        .then(function (second) {
+                            expect(second.hash).toBeDefined();
+                            expect(second.url).toBe('http://www.google.com');
+                            expect(second.hits[0]).toEqual({ ip: '127.0.0.1', realm: 'twitter' });
+                            done();
+                        }).fail(function (err) {
+                            done(err);
+                        });
+                })
+                .fail(function (err) {
                     done(err);
                 });
-            });
         });
 
         it('Hold information about the user that requests the shortened url multiple times', function (done) {
-            urlShortener.shorten('http://www.google.com', function (err, first) {
-                expect(first.hash).toBeDefined();
-                urlShortener.resolve(first.hash, {ip: '127.0.0.1', realm: 'twitter'}, function () {
-                    urlShortener.resolve(first.hash, {ip: '127.0.0.2', realm: 'google'}, function (err, second) {
-                        expect(second.hits[0]).toEqual({ ip: '127.0.0.1', realm: 'twitter' });
-                        expect(second.hits[1]).toEqual({ ip: '127.0.0.2', realm: 'google' });
-                        expect(second.totalHits).toBe(2);
-                        done(err);
-                    });
-                });
-            });
+            urlShortener.shorten('http://www.google.com')
+                .then(function (first) {
+                    expect(first.hash).toBeDefined();
+                    return [
+                        urlShortener.resolve(first.hash, {ip: '127.0.0.1', realm: 'twitter'}),
+                        urlShortener.resolve(first.hash, {ip: '127.0.0.2', realm: 'google'})
+                    ];
+                })
+                .spread(function (first, second) {
+                    expect(second.hits[0]).toEqual({ ip: '127.0.0.1', realm: 'twitter' });
+                    expect(second.hits[1]).toEqual({ ip: '127.0.0.2', realm: 'google' });
+                    expect(second.totalHits).toBe(2);
+                    done();
+                })
+                .fail(done);
         });
-
     });
 });
